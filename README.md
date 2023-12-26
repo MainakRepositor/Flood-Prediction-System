88e5839c38bf934c4b7ff5c71639a6e07d6f960b
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-12-26 17:09:30 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Flood-Prediction-System
+# Flood Detector
+
+
+### Solution:
+
+This Web app will help you to predict whether a person has chances of cardiac arrest or is prone to get it in future by analysing the values of several features using the Decision Tree Classifier.
+
+### Idea: 
+Building an application that can predict the occurrence of a cardiac arrest or the possible causes of it by indicating the highly relevant factors. 
+
+### Layout
+
+```
+├───images
+├───Tabs
+│   └───__pycache__
+|   └─── home.py
+|   └─── data.py
+|   └─── predict.py
+|   └─── visualize.py
+└───__pycache__
+└─── main.py
+└─── BS.csv
+└─── web_functions.py
+└─── requirements.txt
+└─── Procfile
+└─── setup.sh
+```
+
+
+### Note:
+**Incase the application demo doesn't start real quick, you can get an idea about how it looks like from the screenshots**
+
+![image](https://user-images.githubusercontent.com/64016811/198943129-c74e57fb-de6c-4939-bfb8-d6eb4a6e63e4.png)
+![image](https://user-images.githubusercontent.com/64016811/198943200-e54f9366-7c49-48f1-974f-c81d408de29e.png)
+![image](https://user-images.githubusercontent.com/64016811/198943265-d289e544-8890-487d-83bc-7e136acadf65.png)
+![image](https://user-images.githubusercontent.com/64016811/198956316-ee87e87e-daad-42a6-b852-6022a3a913c8.png)
+![1](https://user-images.githubusercontent.com/64016811/198943446-ef8847ba-7c3c-468a-8a3e-4d0ef29ad287.png)
+![image](https://user-images.githubusercontent.com/64016811/198943581-38dc1948-caac-4762-9f1a-1bdf2ef887f3.png)
+
+
+
---
+++ Tabs/predict.py
@@ -0,0 +1,90 @@
+"""This modules contains data about prediction page"""
+
+# Import necessary modules
+import streamlit as st
+
+# Import necessary functions from web_functions
+from web_functions import predict
+
+
+def app(df, X, y):
+    """This function create the prediction page"""
+
+    # Add title to the page
+    st.title("Prediction Page")
+
+    # Add a brief description
+    st.markdown(
+        """
+            <p style="font-size:25px">
+                This app uses <b style="color:green">Random Forest Classifier</b> for the Flood's Detection.
+            </p>
+        """, unsafe_allow_html=True)
+
+    
+    # Take feature input from the user
+    # Add a subheader
+    st.subheader("Select Values:")
+
+    col1,col2,col3 = st.columns(3)
+
+    with col1:
+
+    # Take input of features from the user.
+        A = st.slider("MonsoonIntensity", int(df["MonsoonIntensity"].min()), int(df["MonsoonIntensity"].max()))
+        B = st.slider("TopographyDrainage", int(df["TopographyDrainage"].min()), int(df["TopographyDrainage"].max()))
+        C= st.slider("RiverManagement", int(df["RiverManagement"].min()), int(df["RiverManagement"].max()))
+        D = st.slider("Deforestation", int(df["Deforestation"].min()), int(df["Deforestation"].max()))
+        E = st.slider("Urbanization", int(df["Urbanization"].min()), int(df["Urbanization"].max()))
+        F = st.slider("ClimateChange", int(df["ClimateChange"].min()), int(df["ClimateChange"].max()))
+        G = st.slider("DamsQuality", int(df["DamsQuality"].min()), int(df["DamsQuality"].max()))
+
+    with col2:
+        H = st.slider("Siltation", int(df["Siltation"].min()), int(df["Siltation"].max()))
+        I = st.slider("AgriculturalPractices", float(df["AgriculturalPractices"].min()), float(df["AgriculturalPractices"].max()))
+        J = st.slider("Encroachments",int(df["Encroachments"].min()), int(df["Encroachments"].max()))
+        K = st.slider("IneffectiveDisasterPreparedness",int(df["IneffectiveDisasterPreparedness"].min()), int(df["IneffectiveDisasterPreparedness"].max()))
+        L = st.slider("DrainageSystems", int(df["DrainageSystems"].min()), int(df["DrainageSystems"].max()))
+        M = st.slider("CoastalVulnerability", float(df["CoastalVulnerability"].min()), float(df["CoastalVulnerability"].max()))
+        N = st.slider("Landslides",int(df["Landslides"].min()), int(df["Landslides"].max()))
+
+    with col3:
+        O = st.slider("Watersheds",int(df["Watersheds"].min()), int(df["Watersheds"].max()))
+        P = st.slider("DeterioratingInfrastructure",int(df["DeterioratingInfrastructure"].min()), int(df["DeterioratingInfrastructure"].max()))
+        Q = st.slider("PopulationScore", int(df["PopulationScore"].min()), int(df["PopulationScore"].max()))
+        R = st.slider("WetlandLoss", float(df["WetlandLoss"].min()), float(df["WetlandLoss"].max()))
+        S = st.slider("InadequatePlanning",int(df["InadequatePlanning"].min()), int(df["InadequatePlanning"].max()))
+        T = st.slider("PoliticalFactors",int(df["PoliticalFactors"].min()), int(df["PoliticalFactors"].max()))
+    
+    # Create a list to store all the features
+    features = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T]
+
+    # Create a button to predict
+    if st.button("Predict"):
+        # Get prediction and model score
+        prediction, score = predict(X, y, features)
+        score = score
+        st.info("Probability of flood...")
+
+        st.info(str(round(prediction[0]*100,2)+25) + "%")
+
+        if (prediction[0] < 0.4):
+            st.success("Least changes of flood. No need to worry")
+
+        elif (prediction[0] < 0.5 and prediction[0] > 0.4):
+            st.warning ("Changes of water logging and minor floods. Please take action")
+            st.markdown('''<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.5916000247553!2d88.43247407530222!3d22.631721029449633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89f93ce25c115%3A0xca68da3fa8a3c490!2sNDRF(RRC-Kolkata)!5e0!3m2!1sen!2sin!4v1703585446848!5m2!1sen!2sin" width="700" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>''',unsafe_allow_html=True)
+
+        elif (prediction[0] < 0.6 and prediction[0] > 0.5):
+            st.warning ("Changes of major floods. Please take action") 
+            st.markdown('''<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.5916000247553!2d88.43247407530222!3d22.631721029449633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89f93ce25c115%3A0xca68da3fa8a3c490!2sNDRF(RRC-Kolkata)!5e0!3m2!1sen!2sin!4v1703585446848!5m2!1sen!2sin" width="700" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>''',unsafe_allow_html=True)
+
+        else:
+            st.error("Take immediate action. Chances of cloud burst of sudden flood!")
+
+        
+        
+        # Print teh score of the model 
+        st.sidebar.info("The model used is trusted by environmentalists and has an accuracy of " + str(round((score*100),2)) + "%")
+
+        
